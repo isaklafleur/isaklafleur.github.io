59661179cc1ac645b0b78ccf32addd429a207a4c
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-01 23:18:47 +0100

    updated to angular 5.0.0 and angular cli 1.5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 
@@ -25,7 +25,7 @@ import { DashboardComponent } from "./components/dashboard/dashboard.component";
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpModule,
+    HttpClientModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: true,
     }),
--- client/src/app/services/hero.service.ts
+++ client/src/app/services/hero.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Hero } from "../classes/hero";
-import { Headers, Http } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 
 import "rxjs/add/operator/toPromise";
 
 @Injectable()
 export class HeroService {
   private heroesUrl = "api/heroes"; // URL to web api
-  private headers = new Headers({ "Content-Type": "application/json" });
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
 
   getHero(id: number): Promise<Hero> {
     const url = `${this.heroesUrl}/${id}`;
@@ -16,7 +15,7 @@ export class HeroService {
     return this.http
       .get(url)
       .toPromise()
-      .then(response => response.json().data as Hero)
+      .then(response => response["data"] as Hero)
       .catch(this.handleError);
   }
 
@@ -24,31 +23,29 @@ export class HeroService {
     return this.http
       .get(this.heroesUrl)
       .toPromise()
-      .then(response => response.json().data as Hero[])
+      .then(response => response["data"] as Hero[])
       .catch(this.handleError);
   }
 
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
-      .put(url, JSON.stringify(hero), { headers: this.headers })
+      .put(url, hero)
       .toPromise()
       .then(() => hero)
       .catch(this.handleError);
   }
   create(name: string): Promise<Hero> {
     return this.http
-      .post(this.heroesUrl, JSON.stringify({ name: name }), {
-        headers: this.headers,
-      })
+      .post(this.heroesUrl, { name: name })
       .toPromise()
-      .then(res => res.json().data as Hero)
+      .then(response => response["data"] as Hero)
       .catch(this.handleError);
   }
   delete(id: number): Promise<void> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http
-      .delete(url, { headers: this.headers })
+      .delete(url)
       .toPromise()
       .then(() => null)
       .catch(this.handleError);
